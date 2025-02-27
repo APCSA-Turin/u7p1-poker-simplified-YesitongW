2f703edd0f4989dd83e16159322c3976a6ba27e3
YesitongW <wuy2@nycstudents.net>
2025-02-27 14:25:56 +0000

    w2

--- src/main/java/com/example/project/Deck.java
+++ src/main/java/com/example/project/Deck.java
@@ -15,16 +15,28 @@ public class Deck{
         return cards;
     }
 
-    public  void initializeDeck(){ //hint.. use the utility class
+    public void initializeDeck() {
+        String[] suits = Utility.getSuits();
+        String[] ranks = Utility.getRanks();
         
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                Card card = new Card(rank, suit);
+                cards.add(card);
+            }
+        }
     }
+    
 
     public  void shuffleDeck(){ //You can use the Collections library or another method. You do not have to create your own shuffle algorithm
-        
+        Collections.shuffle(cards);
     }
 
     public  Card drawCard(){
-       return new Card("","");
+        if (!isEmpty()) {
+            return cards.remove(cards.size() - 1); // Draw the top card
+        }
+        return null; // Return null if the deck is empty
     }
 
     public  boolean isEmpty(){
